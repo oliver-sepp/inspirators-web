f3f3e172590e4c1618b92768bd4e35cb40fa5c3f
Oliver Sepp <olsepp@taltech.ee>
2025-11-04 14:50:21 +0200

    vector layer improvement

--- test/src/lib/components/shared/VectorLayer.svelte
+++ test/src/lib/components/shared/VectorLayer.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
-  // Import all SVG files dynamically from the folder
-  const vectorModules = import.meta.glob('$lib/assets/vectors/*.svg', { eager: true });
 import newVector from '$lib/assets/new_vectors/final.svg';
-  // Extract URLs from the imported modules
-  const vectors = Object.values(vectorModules).map((mod: any) => mod.default);
 </script>
 
 <div class="relative w-full h-screen bg-transparent">
   
-  <img class="opacity-90" src={newVector} alt="">
+  <img class="w-full object-cover opacity-90" src={newVector} alt="Vectors Background"/>
   
-  <!-- {#each vectors as vector}
-    <img
-      src={vector}
-      alt="Vector"
-      class="absolute inset-0 w-full max-h-[150vh] opacity-15"
-    />
-  {/each} -->
 </div>
